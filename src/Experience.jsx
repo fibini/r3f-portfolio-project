346c7243a2eb1182e468f4d743887c96ba8f9045
fibini <fabienswift@gmail.com>
2023-02-04 13:19:29 -0400

    Add light emitting from the screen

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -22,6 +22,14 @@ export default function Experience()
             snap={{ mass: 4, tension: 400 }}
         >
             <Float rotationIntensity={ 0.4 }>
+                <rectAreaLight
+                    width={2.5}
+                    height={1.65}
+                    intensity={80}
+                    color={'#191765'}
+                    rotation={[0.1, Math.PI, 0]}
+                    position={[0, 0.55, -1.15]}
+                 />
                 <primitive 
                     object={ computer.scene }
                     position-y={ -1.2 }
