48c18b14f0ddab4f86d7f7ab8828cee2c436d983
fibini <fabienswift@gmail.com>
2023-02-04 10:57:18 -0400

    Add laptop model and change camera controls

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,15 +1,25 @@
-import { OrbitControls } from '@react-three/drei'
+import { PresentationControls, Float, Environment, useGLTF } from '@react-three/drei'
 
 export default function Experience()
 {
+    const computer = useGLTF("https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf")
+    console.log(computer);
+
     return <>
 
-        <OrbitControls makeDefault />
-        
-        <mesh>
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
+        <Environment preset='city' />
 
+        <color args={ ['#241a1a']} attach="background" />
+
+        {/* <OrbitControls makeDefault /> */}
+
+        <PresentationControls>
+            <Float rotationIntensity={ 0.4 }>
+                <primitive 
+                    object={ computer.scene }
+                    position-y={ -1.2 }
+                />
+            </Float>
+        </PresentationControls>
     </>
 }
