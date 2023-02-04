13fddb55011452f2b54e4937719145f05b2de008
fibini <fabienswift@gmail.com>
2023-02-04 11:10:11 -0400

    Limit camera controls

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -13,7 +13,14 @@ export default function Experience()
 
         {/* <OrbitControls makeDefault /> */}
 
-        <PresentationControls>
+        <PresentationControls 
+            global 
+            rotation={[0.13, 0.1, 0]}
+            polar={[ -0.4, 0.2]}
+            azimuth={[ -1, 0.75]}
+            config={{ mass: 2, tension: 400 }}
+            snap={{ mass: 4, tension: 400 }}
+        >
             <Float rotationIntensity={ 0.4 }>
                 <primitive 
                     object={ computer.scene }
